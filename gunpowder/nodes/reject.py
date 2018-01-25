8a89eb255d441b47aec259b75ef8f7a9058be6d9
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2018-01-25 13:40:23 -0500

    add option for random rejection

--- gunpowder/nodes/reject.py
+++ gunpowder/nodes/reject.py
@@ -1,5 +1,5 @@
 import logging
-
+import random
 from .batch_filter import BatchFilter
 from gunpowder.profiling import Timing
 from gunpowder.volume import VolumeTypes
@@ -8,9 +8,10 @@ logger = logging.getLogger(__name__)
 
 class Reject(BatchFilter):
 
-    def __init__(self, min_masked=0.5, mask_volume_type=VolumeTypes.GT_MASK):
+    def __init__(self, min_masked=0.5, mask_volume_type=VolumeTypes.GT_MASK, reject_probability=1.):
         self.min_masked = min_masked
         self.mask_volume_type = mask_volume_type
+        self.reject_probability = reject_probability
 
     def setup(self):
         assert self.mask_volume_type in self.spec, "Reject can only be used if %s is provided"%self.mask_volume_type
@@ -33,10 +34,14 @@ class Reject(BatchFilter):
             mask_ratio = batch.volumes[self.mask_volume_type].data.mean()
             have_good_batch = mask_ratio>=self.min_masked
 
+            if not have_good_batch and self.reject_probability < 1.:
+                have_good_batch = random.random() > self.reject_probability
+                logger.info("randomly accepted batch with mask ratio %f" %mask_ratio)
+
             if not have_good_batch:
 
                 logger.debug(
-                    "reject batch with mask ratio %f at "%mask_ratio +
+                    "reject batch with mask ratio %f at " % mask_ratio +
                     str(batch.volumes[self.mask_volume_type].spec.roi))
                 num_rejected += 1
 
