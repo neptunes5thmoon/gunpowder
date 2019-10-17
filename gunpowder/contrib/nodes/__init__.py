b71efdc7776525204a10eb13e86aa67a5ba4fe83
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-10-17 15:39:13 -0400

    add node to crop an array

--- gunpowder/contrib/nodes/__init__.py
+++ gunpowder/contrib/nodes/__init__.py
@@ -8,6 +8,7 @@ from .add_boundary_distance_gradients import AddBoundaryDistanceGradients
 from .add_gt_mask_exclusive_zone import AddGtMaskExclusiveZone
 from .add_nonsymmetric_affinities import AddNonsymmetricAffinities
 from .add_vector_map import AddVectorMap
+from .crop_array import CropArray
 from .combine_distances import CombineDistances
 from .dvid_partner_annotation_source import DvidPartnerAnnotationSource
 from .hdf5_points_source import Hdf5PointsSource
